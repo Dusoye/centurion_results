f33b191408d6eff819b7fccd6297a5af0dbcbbf1
dusoye <riz@Rizs-MacBook-Pro.local>
2023-07-26 19:37:54 +0100

    finish markdown

--- results_analysis.R
+++ results_analysis.R
@@ -1,19 +1,25 @@
 library(readxl)
 library(dplyr)
+library(tidyr)
 library(lubridate)
 library(ggplot2)
 library(data.table)
 library(rvest)
 
+# load results data
 times_combined <- data.frame()
 for(i in list.files('./data/')){
   data <- read_excel(paste0('./data/',i)) %>%
     rename_all(~ gsub("\\(.*\\)", "", .)) #remove brackets from some checkpoint names to standardise
+  data <- data %>%
+    mutate(year = substr(i,0,4)) %>%
+    select(year, Bib, Runner, Sex, Category, everything())
+  
   times_combined <- bind_rows(times_combined, data, .id = 'id')
   rm(data)
 }
 
-# Function to extract the checkpoint names and distnaces from an event
+# Extract the checkpoint names and distances from an event
 scrape.distance <- function(event){
   url = paste0('https://centurionrunning.com/raceresult/',event)
   webpage <- read_html(url)
@@ -22,7 +28,6 @@ scrape.distance <- function(event){
   td_content <- webpage %>%
     html_nodes("td.nopadding") %>%
     html_text()
-  
   
   td_content <- head(td_content, 11)
   
@@ -39,56 +44,59 @@ scrape.distance <- function(event){
 
 distances <- scrape.distance('612e642b4bd9d6da4fb09a18')
 
-
-times_combined %>%
-  #mutate_at(vars(8:20), hms) %>%
-  select(colnames(data)) %>%
-  mutate(runner = paste0(Bib,"_", Runner)) %>%
-  select(c(runner,7:14)) %>%
-  reshape2::melt(., id.var = 'runner') %>%
-  mutate(value = hms(value)) %>%
-  ggplot(aes(x = variable, y = value, colour = runner)) +
-  geom_line() +
-  theme_minimal()
-
-
-times_combined %>%
-  #mutate_at(vars(8:20), hms) %>%
-  select(colnames(data)) %>%
-  mutate(runner = paste0(Bib,"_", Runner)) %>%
-  select(c(runner,7:14)) %>%
-  reshape2::melt(., id.var = 'runner') %>%
-  mutate(value = hms(value),
-         value_mins = period_to_seconds(value)/60) %>%
-  merge(., distances, by.x= 'variable', by.y = 'checkpoint') %>%
-  mutate(mpk = value_mins / value.y) -> speed_data
-
+speed_data <- times_combined %>%
+  filter(Ashford != 'N/A') %>% # remove DNF runners
+  mutate(runner = paste0(year, "_", Bib,"_", Runner)) %>%
+  select(-c(id, Bib, Runner, Category, `Date Of Birth`, Position)) %>% 
+  pivot_longer(cols = 3:18, names_to = 'checkpoint', values_to = 'time') %>% 
+  merge(., distances, by = 'checkpoint') %>%
+  mutate(time = hms(time),
+         time_mins = period_to_seconds(time)/60,
+         mins_per_km = time_mins/km)
 
 speed_data %>%
-  select(runner, variable, value_mins) %>%
-  pivot_wider(names_from = variable, values_from = value_mins) %>%
+  select(runner, checkpoint, time_mins) %>%
+  pivot_wider(names_from = checkpoint, values_from = time_mins) %>% 
   select(-runner) %>%
-  lm(Eastbourne ~ ., .) -> speed_model
+  lm(Ashford ~ ., .) -> speed_model
 
-speed_data %>%
-  filter(variable == 'Eastbourne') %>%
-  filter(value_mins >= (23*60) & value_mins < (24*60)) %>%
-  select(runner) -> runner_filter
+time_range = c(29,30)
 
-speed_data %>%
-  merge(., runner_filter, by = 'runner') %>%
-  group_by(variable) %>%
-  summarise(mean = mean(value_mins, na.rm = TRUE),
-            max = max(value_mins, na.rm = TRUE),
-            quantile = quantile(value_mins, p =0.75, na.rm = TRUE),
-            mean_hms = seconds_to_period(seconds(mean*60)),
-            max_hms = seconds_to_period(seconds(max*60)),
-            q_hms = seconds_to_period(seconds(quantile*60))) %>%
-  arrange(mean)
+summary.table <- function(mintime, maxtime){
+  speed_data %>%
+    filter(checkpoint == 'Ashford') %>%
+    filter(time_mins >= (mintime*60) & time_mins < (maxtime*60)) %>%
+    select(runner) -> runner_filter
+  
+  speed_data %>%
+    merge(., runner_filter, by = 'runner') %>%
+    group_by(checkpoint) %>%
+    summarise(mean = round(mean(time_mins, na.rm = TRUE),1),
+              min = min(time_mins, na.rm = TRUE),
+              max = max(time_mins, na.rm = TRUE),
+              quantile = quantile(time_mins, p =0.75, na.rm = TRUE),
+              mean_hms = seconds_to_period(seconds(mean*60)),
+              min_hms = seconds_to_period(seconds(min*60)),
+              max_hms = seconds_to_period(seconds(max*60))) %>%
+    select(-c(mean, min, max, quantile)) %>%
+    arrange(mean_hms) -> table.out
+  
+  return(table.out)
+}
 
-speed_data %>%
-  merge(., runner_filter, by = 'runner') %>%
-  arrange(value.y) %>%
-  ggplot(aes(x = variable, y = mpk)) +
-  geom_boxplot() +
-  theme_minimal()
+summary.plot <- function(mintime, maxtime){
+  speed_data %>%
+    filter(checkpoint == 'Ashford') %>%
+    filter(time_mins >= (mintime*60) & time_mins < (maxtime*60)) %>%
+    select(runner) -> runner_filter
+  
+  speed_data %>%
+    merge(., runner_filter, by = 'runner') %>% 
+    mutate(checkpoint = reorder(checkpoint, km)) %>%
+    ggplot(aes(x = checkpoint, y = mins_per_km, colour = Sex)) +
+    geom_boxplot() +
+    theme_minimal() +
+    ggtitle(paste0('Finishers between ', mintime,":00 and ", maxtime,":00")) -> plot.out
+  
+  return(plot.out)
+}
